09dccbb2c59c47df664bf3df8b681bd495a13ed6
André Silva <andre.silva@posteo.de>
2023-03-02 17:12:22 +0100

    Initial commit

---
+++ recap-project-2_quiz-app/index.js
@@ -0,0 +1,27 @@
+const bookmarkButton = document.querySelector('[data-js="question-card__icon"]');
+const answerButton = document.querySelector ('[data-js="question-card_button"]');
+const answerText = document.querySelector('[data-js="question-card__answer"]');
+
+
+
+
+
+bookmarkButton.addEventListener("click", () => {
+    if (bookmarkButton.getAttribute("src")==="./assets/bookmark.png") {
+        bookmarkButton.setAttribute("src", "./assets/bookmark_filled.png");
+    }else{
+        bookmarkButton.setAttribute("src", "./assets/bookmark.png");
+    }
+})
+
+
+answerButton.addEventListener("click", () => {
+answerText.classList.toggle("question-card__answer_hidden");
+
+
+
+})
+
+
+
+
