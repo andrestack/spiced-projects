2abcd3cddebf8ed3c9ade4012d20675b75e55fb5
André Silva <andre.silva@posteo.de>
2023-03-03 10:55:41 +0100

    Add for presentation

--- recap-project-2_quiz-app/index.js
+++ recap-project-2_quiz-app/index.js
@@ -2,7 +2,9 @@ const bookmarkButton = document.querySelector('[data-js="question-card__icon"]')
 const answerButton = document.querySelector ('[data-js="question-card_button"]');
 const answerText = document.querySelector('[data-js="question-card__answer"]');
 
-bookmarkButton.addEventListener("click", () => {
+
+
+bookmarkButton && bookmarkButton.addEventListener("click", () => {
     if (bookmarkButton.getAttribute("src")==="./assets/bookmark.png") {
         bookmarkButton.setAttribute("src", "./assets/bookmark_filled.png");
     }else{
@@ -11,7 +13,7 @@ bookmarkButton.addEventListener("click", () => {
 })
 
 
-answerButton.addEventListener("click", () => {
+answerButton && answerButton.addEventListener("click", () => {
 answerText.classList.toggle("question-card__answer_hidden");
 textDisplay();
 
@@ -28,15 +30,50 @@ function textDisplay () {
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (event) => {
+form && form.addEventListener("submit", (event) => {
 event.preventDefault();
+event.target.remove();
 // const formElements = event.target.elements;
 const formData = new FormData(event.target);
 const data = Object.fromEntries(formData);
 // console.log(data);
 })
 
-const newCard = document.createElement(".section question-card");
+// const newCard = document.createElement(".section question-card");
 
-document.body.append(newCard);
-console.log(newCard);
+// document.body.append(newCard);
+// console.log(newCard);
+
+const questionField = document.querySelector('[data-js="question-text"]');
+const answerField = document.querySelector('[data-js="answer-text"]');
+const questionCharacters = document.querySelector('[data-js="question-characters"]');
+const answerCharacters = document.querySelector('[data-js="answer-characters"]');
+
+questionField && questionField.addEventListener("input", (event)=>{
+charactersLeft(event);
+})
+
+answerField && answerField.addEventListener("input", (event)=>{
+charactersLeft(event);
+
+})
+
+function charactersLeft (event) {
+    const maxLength = event.target.maxLength;
+    const amountLeft = (maxLength - event.target.value.length);
+    console.log("hello");
+    
+    if (event.target.name === "question") {
+        questionCharacters.textContent = amountLeft + " characters left"
+}else{
+    answerCharacters.textContent = amountLeft + " characters left"
+}    
+}
+
+
+
+// const lengthLeft = questionField.value;
+
+//     console.log(lengthLeft);
+
+// })
