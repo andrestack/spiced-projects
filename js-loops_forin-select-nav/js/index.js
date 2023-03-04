6d92d01855cb4e1f0811c427c9b25c8516e5962b
André Silva <andre.silva@posteo.de>
2023-03-04 23:05:45 +0100

    add last changes

--- js-loops_forin-select-nav/js/index.js
+++ js-loops_forin-select-nav/js/index.js
@@ -20,28 +20,21 @@ const select = document.createElement("select");
 select.name = "languages";
 main.append(select);
 
-// const cohort={
-//   name: "sugar",
-//   amountStundent: 9,
-//   academy: Spiced,
-//   };
-  
-//   for (let key in cohort){
-//   console.log(key, cohort[key]);
-//   --- console.log (cohort.key); we cannot acce like this, bc key is a variable --
-
-// --v-- write/change code here --v--
-for (let key in languages){
-  console.log(key, languages[key]);
+for (let sprache in languages){
   const option = document.createElement("option");
-  option.innerHTML = languages[key];
+  option.textContent = languages[sprache];
   select.append(option);
+  
 }
 
+// // --^-- write/change code here --^--
 
-// --^-- write/change code here --^--
+// // Part 2: Creating a Navigation Bar
+const navElement = document.createElement("nav");
+const ul = document.createElement("ul");
+main.append(navElement);
+navElement.append(ul);
 
-// Part 2: Creating a Navigation Bar
 
 const nav = {
   home: { href: "/home", text: "Home" },
@@ -49,24 +42,15 @@ const nav = {
   contact: { href: "/contact", text: "Contact" },
 };
 
-const navElement = document.createElement("nav");
-const ul = document.createElement("ul");
-main.append(navElement);
-navElement.append(ul);
-
-// --v-- write/change code here --v--
-
-for (let items in nav){
-  console.log(nav[items]);
+for (let key in nav){
   const li = document.createElement("li");
-  const a = document.createElement("a")
-  const element = nav[items];
+  const a = document.createElement("a");
+  const element = nav[key];
   a.setAttribute("href", element.href);
   a.textContent = element.text;
-  
-ul.append(li);
-li.append(a);  
+  ul.append(li);
+  li.append(a);
 }
 
 
-// --^-- write/change code here --^--
+
